c419462ace8566e13914e2b4dcbdbad8c0e344f3
David Keegan <keegan3d@gmail.com>
2010-03-08 01:57:08 -0800

    adding a couple gml files

--- libdev.py
+++ libdev.py
@@ -2,15 +2,15 @@ from PyGML import GML
 import urllib
 #this file is only for testing the library
 if __name__ == '__main__':
-    gmlFile = urllib.urlopen('http://000000book.com/data/154.gml')
-    #gmlFile = open('155.gml', 'r')
+    #gmlFile = urllib.urlopen('http://000000book.com/data/154.gml')
+    gmlFile = open('SampleGML/140.gml', 'r')
     gml = GML(gmlFile)
     gmlFile.close()
     
     print gml.client()
-    print gml.client().get('name')
+    #print gml.client().get('name')
     print gml.environment()
-    print gml.environment().get('screenBounds').x
+    #print gml.environment().get('screenBounds').x
     for stroke in gml.iterStrokes():
         for point in stroke.iterPoints():
             print point
